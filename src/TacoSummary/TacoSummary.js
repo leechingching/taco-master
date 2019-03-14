d75829994ca0ae6b873b7dd91276c65075d1ad94
Ching Ching Lee <allisonchinglee@gmail.com>
2019-03-14 23:03:50 +0800

    updated app

--- src/TacoSummary/TacoSummary.js
+++ src/TacoSummary/TacoSummary.js
@@ -1,26 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const TacoSummary = props => {
 
+    const [ingredient, setIngredient] = useState([])
+    const [userClick, setUserClicked] = useState(false)
+    const [loading, setLoading] = useState(false)
+
     useEffect(() => {
-        let config = {
-            headers: { 
-                'Content-Type': 'application/json', 
-                'X-RapidAPI-Key': process.env.REACT_APP_FOOD_API_KEY }
-          };
-        axios.post('https://zestful.p.rapidapi.com/parseIngredients', config)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(console.log('failed to get the API'))
+        
+        
     }, [])
+
+    function randomRecipe(event) {
+        setLoading(true)
+        const APP_ID = process.env.REACT_APP_ID
+        let foodArr = ['chicken','beef','Eggs', 'Dairy','Vegan', 'paleo','Carbs','pork','Diet','Health','soy','sugar']
+
+        let randomSearch = Math.floor(Math.random()*foodArr.length)
+
+        axios.get(`https://api.edamam.com/search?q=${randomSearch}&app_id=e5522ebc&app_key=${APP_ID}`)
+            .then(res => {
+                const recipes = res.data.hits
+                // setIngredient(recipes)
+                // return recipes
+                let randomNum = Math.floor(Math.random()*recipes.length)
+                setIngredient(
+                    (recipes[randomNum].recipe.label) + ' Taco'
+                )
+                setUserClicked(true)
+                setLoading(false)
+            })
+            setUserClicked(false)
+    }
+
+    let modal = null
+
+    if(loading) {
+        modal = (<p>It is Loading</p>)
+    }
+
 
     return (
         <>
+            
             <h1>Create your Taco</h1>
-            <p>Your Taco: <span></span></p>
-            <button>Generate Your Taco!</button>
+            { userClick && !loading ? <p>Your Taco: {ingredient}<span></span></p> : null }
+            { loading && !userClick ? <p>It is Loading</p> : null }
+            
+            {/* { modal }  */}
+            <button onClick={() => randomRecipe()}>Generate Your Taco!</button>
         </>
     )
 }
